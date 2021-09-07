029d241ec026d236de983b5badb6f420ba39295a
Radoncode <relbouni69@gmail.com>
2021-09-07 18:38:57 +0100

    adding user schema model

---
+++ backend/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const mongooseUniqueValidator = require('mongoose-unique-validator');
+
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+    email: { type: String, required:true, unique: true },
+    password: { type: String, required: true }
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
